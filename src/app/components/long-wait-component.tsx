b5566abd6b92124df86915031b8f846339e060aa
santell <justintouchstone29@gmail.com>
2023-12-20 20:24:21 -0300

    Criando estrutura do app

--- src/app/components/long-wait-component.tsx
+++
@@ -1,5 +0,0 @@
-export async function LongWaitComponent() {
-  await new Promise((resolve) => setTimeout(resolve, 5000))
-
-  return <p>Carregado!</p>
-}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,11 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    fontFamily: {
+      sans: 'var(--font-inter)',
+    },
+  },
   plugins: [],
 }
 export default config
